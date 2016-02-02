ad9ed0ea1a3a379bddc201a2a23ff440fb4096e6
Justin Geeslin <justin@geesl.in>
2016-02-02 01:54:11 -0600

    Testing Jump to Top scroll

--- tests/JumpToTop.js
+++ tests/JumpToTop.js
@@ -1,7 +1,12 @@
-describe('getDiv', function() {
+describe('JumpToTop', function() {
     var theJumpToTop = sensible.classes.JumpToTop();
-		var el = document.querySelector(".jump-to-top");
     it('Should exist', function() {
-        expect(el.nodeName).toBe('DIV');
+        expect(theJumpToTop.el[0].nodeName).toBe('DIV');
     });
+	
+		theJumpToTop.el.trigger('click');
+	
+		it('Should scroll the window to the top onClick', function() {
+				expect($(window).scrollTop()).toBe(0);
+		});
 });
