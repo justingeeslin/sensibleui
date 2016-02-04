65ffd05f7da2bb6dd4fe2d3e401a62e1e3334bab
Justin Geeslin <justin@geesl.in>
2016-02-04 01:25:52 -0600

    Creating examples js

--- examples/_.js
+++ examples/_.js
@@ -1,2 +1,11 @@
 // Can we just talk about how much sense this makes?
 var myInputDelete = sensible.classes.InputDelete();
+//Label it Clearable Input box
+$(myInputDelete.el).before('<h3>Clearable Input box</h3>');
+
+//Create a Jump to Top
+var myJump = sensible.classes.JumpToTop();
+
+//Create an Expand/Collapse
+var myExpandCollapse = sensible.classes.ExpandCollapse();
+$(myExpandCollapse.el).before('<h3>Expand / Collapse</h3>');
