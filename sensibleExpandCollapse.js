7438af0a08c0c7a0787ea7c670911279b7181a2f
Justin Geeslin <justin@geesl.in>
2016-02-04 01:13:16 -0600

    Issue with Force-closing, Moving to opts

--- sensibleExpandCollapse.js
+++ sensibleExpandCollapse.js
@@ -1,14 +1,14 @@
 var sensible = sensible !== undefined ? sensible : {};
 sensible.classes = sensible.classes !== undefined ? sensible.classes : {};
 
-sensible.classes.ExpandCollapse = function (title, content, slug) {
+sensible.classes.ExpandCollapse = function (opts) {
 	var self = this;
-			
+	
 	//Argument Handling
-	var title = typeof title != "undefined" ? title : 'Untitled Question?';
-	var content = typeof content != "undefined" ? content : 'Untitled Answer.';
-	this.slug = typeof slug != "undefined" ? slug : 'untitled-question';
-
+	var title = opts && opts.title ? opts.title : 'Untitled Question?';
+	var content = opts && opts.content ? opts.content : 'Untitled Answer.';
+	this.slug = opts && opts.slug ? opts.slug : 'untitled-question';
+	
 	//Capture the element's state
 	this.isOpen = false;
 
@@ -23,14 +23,17 @@ sensible.classes.ExpandCollapse = function (title, content, slug) {
 		e.preventDefault();
 		this.isOpen = !this.isOpen;
 
+		var answer = self.el.find('a[href="#!' + self.slug + '"]').next();
+		
 		if (this.isOpen) {
 			console.log('Opened');
+			answer.show()
 		}
 		else {
 			console.log('Closed');
+			answer.hide()
 		}
 		
-		self.el.find('a[href="#!' + self.slug + '"]').next().toggle();
 		//If the question is opened..
 		if (this.isOpen) {
 			//.. close all the other questions
@@ -40,19 +43,21 @@ sensible.classes.ExpandCollapse = function (title, content, slug) {
 	}
 
 	this.forceCloseAll = function(e, clicked) {
+		
 		if (clicked !== undefined) {
-			var questionClicked = $(clicked).attr('href')
+			var questionClicked = $(clicked).attr('href');
 			
 			if (questionClicked) {
-				questionClicked.replace('#!','');	
+				questionClicked = questionClicked.replace('#!','');	
 			}
-			
+
 			//If the question clicked is myself..
 			if (questionClicked == self.slug) {
 				//..Do not force close
 			}
 			else {
 				//Force close	
+				console.log('Forcing Close: ' + self.slug);
 				self.el.find(' a[href="#!' + self.slug + '"]').next().toggle(false);
 				this.isOpen = false;
 			}
@@ -66,6 +71,9 @@ sensible.classes.ExpandCollapse = function (title, content, slug) {
 
 	//Expose an event to toggle the activation. Maybe called when a screen un-slides to close it.
 	$(this.el).on('toggleActivate', this.activate);
+	
+	var target = opts && opts.target ? opts.target : $(document.body);
+	target.append(this.el);
 	
 	return this;
 }
