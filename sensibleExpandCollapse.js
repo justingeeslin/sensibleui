d4780fa9e8777e3ea4e78b337f3a9f4e4c9283ee
Justin Geeslin <justin@geesl.in>
2016-08-23 18:08:13 -0500

    Updating Expand Collapse

--- sensibleExpandCollapse.js
+++ sensibleExpandCollapse.js
@@ -3,78 +3,75 @@ sensible.classes = typeof sensible.classes !== "undefined" ? sensible.classes : 
 
 sensible.classes.ExpandCollapse = function (opts) {
 	var self = this;
-	
-	this.title = "Untitled"
-	this.content = "Untitled Body."
-	this.slug = "untitled"
-	this.url = this.slug
-	
-	$.extend(this, opts);
+
+	var defaults = {
+		title : "Untitled",
+		content : "Untitled Body.",
+		slug : "untitled",
+		target : $(document.body)
+	};
+
+	$.extend(this, defaults, opts);
 
 	console.log("Creating an Expand/Collapse: " + this.title);
+	console.log(this);
 
-	this.el = $('<span>');
-	$(this.el).append('<a href="#' + this.url + '">' + this.title + '</a>');
-	$(this.el).append('<div style="display:none;">' + this.content + '</div>');
+	this.url = function() {
+		return self.slug;
+	}
+
+	this.el = $('<span></span>');
+	this.el.append('<a href="#' + this.url() + '">' + this.title + '</a>');
+	var answer = $('<div style="display:none;">' + this.content + '</div>');
+	this.el.append(answer);
 
 	//Handles expanding and collapsing
-	this.activate = function(e) {
+	this.toggle = function(e) {
+		//No need for this to bubble
 		e.preventDefault()
+
+		//Update the URL incase the windows is refreshed
 		history.replaceState(null, null, $(this).attr('href') )
-		
-		console.log('Original Expanding ' + self.slug);
-		var answer = $(this).next();
-		
+
+		console.log('Toggling: ' + self.slug);
+
 		if (!self.isOpen()) {
-			console.log('Opened');
-			console.log(answer);
-			answer.show()
-			
-			//.. close all the other questions
-			self.el.trigger('expandCollapseClose', this);
+			self.open();
 		}
 		else {
-			console.log('Closed');
-			answer.hide()
+			self.close();
 		}
 
 	}
-	
+
 	this.isOpen = function() {
-		return self.el.find(' a[href="#' + self.url + '"]').next().is(':visible');
+		console.log('This is my element:')
+		console.log(self.el);
+		return self.el.find('div').is(':visible');
 	}
 
-	this.forceCloseAll = function(e, clicked) {
-		console.log('Forcing Close: ' + self.url + ' because I am ' + self.isOpen());
-		if (self.isOpen()) {
-			if (clicked !== undefined) {
-				var questionClicked = $(clicked).attr('href');
-
-				if (questionClicked) {
-					questionClicked = questionClicked.replace('#','');	
-				}
-
-				//If the question clicked is myself..
-				if (questionClicked == self.url) {
-					//..Do not force close
-				}
-				else {
-						//Force close	
-						console.log('Forcing Close: ' + self.url);
-						self.el.find(' a[href="#' + self.url + '"]').next().toggle(false);
-				}
-			}
-		}
-
+	this.close = function() {
+		console.log('Closing: ' + self.slug);
+		answer.hide()
 	}
 
-	$(this.el).on('click', ' > a', this.activate);
+	this.open = function() {
+		console.log('Opening: ' + self.slug);
+		answer.show()
+	}
 
-	//Expose an event to toggle the activation. Maybe called when a screen un-slides to close it.
-	$(this.el).on('toggleActivate', this.activate);
-	
-	var target = this.target ? this.target : $(document.body);
-	target.append(this.el);
-	
+	$(this.el).on('click', ' > a', this.toggle);
+
+	//Expose an events..
+	// ...to toggle the activation. Maybe called when a screen un-slides to close it.
+	$(this.el).on('toggle', this.toggle);
+	// ...to close
+	$(this.el).on('close', this.close);
+	// ...to open
+	$(this.el).on('open', this.open);
+
+	//Append to the Document or whatever
+	this.el.appendTo(this.target);
+
 	return this;
 }
--- tests/ExpandCollapse.js
+++ tests/ExpandCollapse.js
@@ -1,14 +1,42 @@
 describe('ExpandCollapse', function() {
-    var myExpandCollapse = sensible.classes.ExpandCollapse();
-	
+    var myExpandCollapse = new sensible.classes.ExpandCollapse();
+    var myExpandCollapse2 = new sensible.classes.ExpandCollapse({slug: 'untitled2'});
+
 		var theQuestion = myExpandCollapse.el.find('a');
+    var theAnswer = myExpandCollapse.el.find('div');
+
+    var isOpen = function() {
+      return theAnswer.filter(':visible').length > 0
+    }
+
     it('Should exist and be named Untitled', function() {
-        expect(theQuestion.attr('href')).toBe('#!untitled-question');
+        expect(theQuestion.attr('href')).toBe('#untitled');
     });
-	
-		theQuestion.trigger('click');
-	
-		it('Answer should be visible on click.', function() {
-				expect(theQuestion.next().filter(':visible').length > 0 ).toBe(true);
+
+		it('should show content on click.', function() {
+      theQuestion.trigger('click');
+			expect(isOpen()).toBe(true);
 		});
+
+    it('should hide content on second click', function() {
+      theQuestion.trigger('click');
+			expect(isOpen()).toBe(false);
+		});
+
+    it('should close when triggering the toggle event', function() {
+      myExpandCollapse.el.trigger('close');
+			expect(isOpen()).toBe(false);
+		});
+
+    it('should open when triggering the toggle event', function() {
+      myExpandCollapse.el.trigger('open');
+			expect(isOpen()).toBe(true);
+		});
+
+    it('should show/hide when triggering the toggle event', function() {
+      var beforeState = isOpen();
+      myExpandCollapse.el.trigger('toggle');
+      var afterState = isOpen();
+      expect(beforeState).toBe(!afterState);
+    });
 });
