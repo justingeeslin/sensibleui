80093fa0b439bed13902f138c17496d32db38fe0
Justin Geeslin <justin@geesl.in>
2016-08-23 18:08:29 -0500

    Fix: Using the new keyword in tests Jump to top

--- tests/JumpToTop.js
+++ tests/JumpToTop.js
@@ -1,11 +1,11 @@
 describe('JumpToTop', function() {
-    var theJumpToTop = sensible.classes.JumpToTop();
+    var theJumpToTop = new sensible.classes.JumpToTop();
     it('Should exist', function() {
         expect(theJumpToTop.el[0].nodeName).toBe('DIV');
     });
-	
+
 		theJumpToTop.el.trigger('click');
-	
+
 		it('Should scroll the window to the top onClick', function() {
 				expect($(window).scrollTop()).toBe(0);
 		});
