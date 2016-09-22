cf424e4dc753afe6cf613e81d537fc9bb1a48d28
Justin Geeslin <justin@geesl.in>
2016-09-22 15:09:56 -0500

    Organizing karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -67,7 +67,11 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome', 'Safari'],
+    browsers: [
+      // 'Firefox',
+      'Chrome',
+      // 'Safari'
+    ],
 
 		customLaunchers: {
 				Chrome_travis_ci: {
