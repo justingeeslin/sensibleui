fba0054168b3e54c973df3e7847f1b45a746d199
Justin Geeslin <justin@geesl.in>
2016-11-02 17:17:08 -0500

    Adding Highlight

---
+++ sensibleHighlight.js
@@ -0,0 +1,79 @@
+var sensible = sensible !== undefined ? sensible : {};
+sensible.classes = sensible.classes !== undefined ? sensible.classes : {};
+
+sensible.classes.Highlight = function (opts, contentTarget) {
+	var self = this;
+
+	var defaults = {
+		target : $(document.body),
+		className : 'highlight',
+		nodeName : 'span',
+		textToHighlight : '',
+	};
+
+	$.extend(this, defaults, opts);
+
+  this.highlight = function (node) {
+		var node = node ? node : this.target[0];
+		var nodeName = this.nodeName
+		var re = this.textToHighlight
+		var className = this.className
+
+		//Case insenstive
+		re = re.toLowerCase()
+
+		if (re.length <= 0) {
+			return;
+		}
+
+		console.log('Highlighting: ' + re);
+
+    if (node.nodeType === 3) {
+			var lowercaseData = node.data.toLowerCase();
+      var match = lowercaseData.match(re);
+      if (match) {
+				console.log('Found a match.')
+	      var highlight = document.createElement(nodeName);
+	      highlight.className = className;
+	      var wordNode = node.splitText(match.index);
+	      wordNode.splitText(match[0].length);
+	      var wordClone = wordNode.cloneNode(true);
+	      highlight.appendChild(wordClone);
+	      wordNode.parentNode.replaceChild(highlight, wordNode);
+	      return 1; //skip added node in parent
+      }
+    } else if ((node.nodeType === 1 && node.childNodes) && // only element nodes that have children
+            !/(script|style)/i.test(node.tagName) && // ignore script and style nodes
+            !(node.tagName === nodeName.toUpperCase() && node.className === className)) { // skip if already highlighted
+				console.log('Lets get highlighting..');
+				for (var i = 0; i < node.childNodes.length; i++) {
+            i += this.highlight(node.childNodes[i]);
+        }
+    }
+		else {
+			console.log('These aren\'t the nodes I\'m use to');
+		}
+    return 0;
+  }
+
+	this.remove = function() {
+		return $(this.target).find("." + this.className).each(function () {
+        var parent = this.parentNode;
+        parent.replaceChild(this.firstChild, this);
+        parent.normalize();
+    }).end();
+	}
+
+	//Upon construction we should remove previous highlights and highlight again
+	this.go = function() {
+		console.log('Removing previous highlights..')
+		this.remove();
+		console.log('Highlighting..')
+		this.highlight();
+	}
+	this.go();
+
+	this.target.on('go', this.go);
+
+	return this;
+}
---
+++ tests/Highlight.js
@@ -0,0 +1,77 @@
+describe('Highlight', function() {
+
+    beforeAll(function() {
+      console.log('Testing Highlight.')
+      container = $('<div id="highlight"></div>');
+      $(document.body).append(container);
+
+      container.append('<p>Wall Street, Big Banks, Charles Koch</p>');
+      container.append('<p>Kochakola, Bank Roll</p>');
+
+      //Add some style so we can tell
+      $('<style>.highlight { background-color:yellow; }</style>').appendTo(document.head);
+
+    });
+
+
+    it('Should highlight (construct)', function() {
+      var options = {
+        target : container,
+        textToHighlight : 'Koch',
+      }
+      theHighlighter = new sensible.classes.Highlight(options);
+
+      expect($('.highlight').length > 0).toBe(true);
+    });
+
+    it('Should remove highlights', function() {
+      theHighlighter.remove()
+
+      expect($('.highlight').length <= 0).toBe(true);
+    });
+
+    it('Should highlight words specified', function() {
+      var options = {
+        target : container,
+        textToHighlight : 'Bank',
+      }
+      var bankHL = new sensible.classes.Highlight(options);
+
+      var highlightedCorrectWords = true;
+
+      //Look for exceptions
+      $('.highlight').each(function() {
+        var highlightedText = $(this).text();
+        if (highlightedText != options.textToHighlight) {
+          console.log('Something went wrong. Highlighted: ' + highlightedText + ' when you should have highlighted ' + options.textToHighlight);
+          highlightedCorrectWords = false;
+          return 0;
+        }
+      });
+
+      expect(highlightedCorrectWords).toBe(true);
+    });
+
+    it('Should highlight words specified testing for automatic highlight removal', function() {
+      var options = {
+        target : container,
+        textToHighlight : 'Street',
+      }
+      var streetHL = new sensible.classes.Highlight(options);
+
+      var highlightedCorrectWords = true;
+
+      //Look for exceptions
+      $('.highlight').each(function() {
+        var highlightedText = $(this).text();
+        if (highlightedText != options.textToHighlight) {
+          console.log('Something went wrong. Highlighted: ' + highlightedText + ' when you should have highlighted ' + options.textToHighlight);
+          highlightedCorrectWords = false;
+          return 0;
+        }
+      });
+
+      expect(highlightedCorrectWords).toBe(true);
+    });
+
+});
