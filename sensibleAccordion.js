781ba8c2d4b1911d08100fd3254ff802278b9fce
Justin Geeslin <justin@geesl.in>
2016-11-03 16:17:09 -0500

    Adding a scroll compensation to Accordion

--- sensibleAccordion.js
+++ sensibleAccordion.js
@@ -4,15 +4,41 @@ sensible.classes = typeof sensible.classes !== "undefined" ? sensible.classes : 
 sensible.classes.Accordion = function (opts) {
 	var self = this;
 
-	var defaults = {};
+	var defaults = {
+		//When large items collaps sometimes the activated item falls out of view. This option stops that from hapening.
+		scrollCompensate: true,
+	};
 
 	$.extend(this, defaults, opts);
 	$.extend(this, new sensible.classes.ExpandCollapse(this));
 
 	var closeOthers = function() {
 		console.log('Accordion: Closing others...');
+
+		if (self.scrollCompensate) {
+			//Do a balancing act. When large items collapse, scroll up by that amount to follow the newly activated item
+			//Get my height..
+			var openedRect = $(this)[0].getBoundingClientRect();
+			openedRect.y = openedRect.top;
+		}
+
 		//Trigger a close on everyone who is open but not me
-		$('.' + self.classes + '.open').not(self.el).trigger('close');
+		$('.' + self.classes + '.open').not(self.el).each(function() {
+
+			if (self.scrollCompensate) {
+				var closingRect = $(this)[0].getBoundingClientRect();
+				closingRect.y = closingRect.top;
+
+				//Is this element above me?
+				if (closingRect.y < openedRect.y) {
+					// scroll up by the height of the collapsing question's body
+					var closingBodyHeight = $(this).children('div').height();
+					window.scrollBy(0, -closingBodyHeight);
+				}
+			}
+
+			$(this).trigger('close');
+		});
 	}
 
 	$(this.el).on('open', closeOthers);
--- tests/Accordion.js
+++ tests/Accordion.js
@@ -6,6 +6,11 @@ describe('Accordion', function() {
     var theQuestion2 = my2.el.find('a');
     theAnswer2 = my2.el.find('div');
 
+    //Make the question bodies large so that we can test scroll things
+    $('<style>.expand-collapse > div { height:100vh; }</style>').appendTo(document.head);
+    //Also make the body large so that the bottom of the window doesn't push us back up
+    $('<style>html,body { height:500vh; }</style>').appendTo(document.head);
+
     var isOpen = function() {
       return theAnswer.filter(':visible').length > 0
     }
@@ -29,4 +34,24 @@ describe('Accordion', function() {
 
       expect(visibleAnswers.length == 1).toBe(true)
     });
+
+    it('should keep the newly expanded question in view despite closing questions', function(done) {
+      $('.expand-collapse:nth(0)').trigger('open');
+
+      setTimeout(function() {
+        //Scroll to the second question
+        $(window).scrollTop($('.expand-collapse:nth(1)').offset().top)
+
+        theQuestion.click();
+
+        //Is the second question in view? aka does it have a y coord in the positive
+        var myRect = theQuestion[0].getBoundingClientRect()
+        myRect.y = myRect.top;
+        console.log(myRect.y)
+        expect(myRect.y >= 0).toBe(true)
+        done()
+      }, 1000)
+
+    });
+
 });
