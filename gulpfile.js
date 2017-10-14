b210ef0f1e0fc4d6535b3ccf14d4a4e1e3c8f48d
Justin Geeslin <justin@geesl.in>
2017-10-14 00:39:31 -0500

    Comiling distributable js with browserify

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,42 @@ var del = require('del');
 
 var karmaServer = require('karma').Server;
 
-// Concatenate & Minify JS
+var gutil = require('gulp-util');
+var watchify = require('watchify');
+var browserify = require('browserify');
+var assign = require('lodash.assign');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+// add custom browserify options here
+var customOpts = {
+  entries: [
+    'index.js',
+	],
+  debug: false,
+  // list: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('sensible.js'))
+    // optional, remove if you don't need to buffer file contents
+    .pipe(buffer())
+    // optional, remove if you dont want sourcemaps
+    // .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+       // Add transformation tasks to the pipeline here.
+    // .pipe(sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('dist/'));
+}
+
+gulp.task('js', bundle);
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
 gulp.task('scripts', function () {
 	return gulp.src([
 		'js/sensibleComponent.js',
@@ -114,7 +149,7 @@ gulp.task('test-debug', function(done) {
 
 // Watch Files For Changes
 gulp.task('watch', function () {
-	gulp.watch('js/*.js', ['scripts']);
+	gulp.watch('js/*.js', ['js']);
 	gulp.watch('css/*.scss', ['sass']);
 	
 	// gulp.watch('tests/*.*', ['test']);
@@ -123,7 +158,7 @@ gulp.task('watch', function () {
 
 // Default Task
 gulp.task('default', [
-	'scripts',
+	'js',
 	'sass',
 	'sass-InputDelete',
 	'watch'
