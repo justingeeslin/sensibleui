586328d7fc40b58734b231337e8d976193765ea1
Justin Geeslin <justin@geesl.in>
2018-07-18 16:07:15 -0500

    Removing target and render from Components

--- js/sensibleComponent.js
+++ js/sensibleComponent.js
@@ -1,12 +1,12 @@
 var extend = require('extend');
+
+// A Sensible Component is a simple element with state.
 var Component = function (options) {
 	var self = this;
 
 	// Use the private members for custom hidden setters and getters.
 	// An identifier for the component's current state.
 	var state = '';
-	// The element to which this component (el) should be rendered/appended to.
-	var target = undefined;
 
 	var defaults = {
 		// To log or not to log..
@@ -25,45 +25,13 @@ var Component = function (options) {
 		},
 		// To avoid collisions and incase you want to namespace individual components
 		eventNamespace: 'sensible',
-		// Call render automatically upon construction becuse sometimes you just want to construct the thing. Disable if the component request data async and should not be show until it is loaded.
-		autoRender: true,
 	};
-
-	// Supply a default target only as a last resort. This way the body isn't selected every time.
-	if (typeof $contentTarget !== "undefined") {
-		defaults.target = $contentTarget;
-	}
-	else if (typeof options !== "undefined" && typeof options.target !== "undefined") {
-		target = options.target
-	}
-	else {
-		target = $(document.body);
-	}
 
 	this.log = function(msg) {
 		if (self.debug) {
 			console.log(msg);
 		}
 	}
-
-	Object.defineProperty(this, 'target', {
-		get: function() {
-			return target;
-		},
-		set: function(arg) {
-			// If the argument is a string, it is a selector convert it to a jQuery object
-			if (typeof arg === "string") {
-				target = $(arg);
-			}
-			else if (arg instanceof jQuery) {
-				target = arg
-			}
-			else {
-				console.warn('Unregonized target selector.', arg);
-			}
-		},
-		enumerable: true
-	});
 
 	Object.defineProperty(this, 'state', {
 		get: function() { return state; },
@@ -90,24 +58,7 @@ var Component = function (options) {
 		this.state = newState;
 	}
 
-	// Append the El with all of its markup and events to the targetEl
-	this.render = function() {
-		self.preload();
-		self.target.trigger('preload.' + self.eventNamespace);
-		self.log('Rendering..');
-		self.target.append(this.el);
-		self.postload();
-		self.target.trigger('postload.' + self.eventNamespace);
-	}
-
-	// Call render automatically upon construction
-	if (this.autoRender) {
-		this.render()
-	}
-
 	return this;
 }
-
-
 
 module.exports = Component;
--- tests/Component.js
+++ tests/Component.js
@@ -11,79 +11,6 @@ describe('Component', function() {
       expect(aComponent instanceof Component).toBe(true)
     });
 
-    it('should render', function() {
-      aComponent = new Component({
-        el: $('<p id="turtles">Teenage Mutant Ninja Turtles</p>')
-      });
-      expect(aComponent.target.find('#turtles').length > 0).toBe(true)
-
-		});
-
-		it('should render manually without autorender', function() {
-      aComponent = new Component({
-        el: $('<p id="pony">My little pony</p>'),
-        autoRender: false
-      });
-      aComponent.render();
-      expect(aComponent.target.find('#pony').length > 0).toBe(true)
-		});
-
-    it('should call preload callback on render', function() {
-      var didPreload = false;
-      aComponent = new Component({
-        el: $('<p id="polly">Polly Pocket</p>'),
-        preload: function() {
-          didPreload = true;
-        }
-      });
-      aComponent.render();
-      expect(didPreload).toBe(true)
-		});
-
-    it('should call postload callback on render', function() {
-      var didPostload = false;
-      aComponent = new Component({
-        el: $('<p id="joe">GI Joe</p>'),
-        preload: function() {
-          didPostload = true;
-        }
-      });
-      aComponent.render();
-      expect(didPostload).toBe(true)
-		});
-
-    it('should trigger preload event on render', function(done) {
-      var didPreload = false;
-      aComponent = new Component({
-        el: $('<p id="polly">Polly Pocket</p>'),
-      });
-
-      aComponent.target.on('preload.sensible', function() {
-        didPreload = true;
-        expect(didPreload).toBe(true)
-        done()
-      })
-
-      aComponent.render();
-
-		});
-
-    it('should trigger postload event on render', function(done) {
-      var didPostload = false;
-      aComponent = new Component({
-        el: $('<p id="polly">Polly Pocket</p>'),
-      });
-
-      aComponent.target.on('postload.sensible', function() {
-        didPostload = true;
-        expect(didPostload).toBe(true)
-        done()
-      })
-
-      aComponent.render();
-
-		});
-
     it('should get/set state', function() {
       aComponent = new Component({
         el: $('<p id="turtles">Teenage Mutant Ninja Turtles</p>')
@@ -138,24 +65,6 @@ describe('Component', function() {
 
 		});
 
-    it('should set target as a string selector', function() {
-      aComponent = new Component({
-        target: 'html',
-        el: $('<p id="turtles">Teenage Mutant Ninja Turtles</p>')
-      });
-      expect(aComponent.target[0]).toBe($('html')[0])
-
-		});
-
-    it('should set target as a jQuery object', function() {
-      aComponent = new Component({
-        target: $('html'),
-        el: $('<p id="turtles">Teenage Mutant Ninja Turtles</p>')
-      });
-      expect(aComponent.target[0]).toBe($('html')[0])
-
-		});
-
     it('should have a state property that is enumerable to be compatible with most extend functions in JavaScript', function() {
       aComponent = new Component({
         el: $('<p id="turtles">Teenage Mutant Ninja Turtles</p>')
@@ -166,14 +75,6 @@ describe('Component', function() {
       }
       expect(arrayProps.indexOf('state') > -1).toBe(true)
 
-		});
-
-    it('should destroy and empty the target', function() {
-      aComponent = new Component({
-        el: $('<p id="cars">Porche</p>')
-      });
-      aComponent.destroy()
-      expect($('#cars').length).toBe(0);
 		});
 
     it('should allow destroy to be overridden', function(done) {
