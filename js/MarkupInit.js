610fb9afac461ad40fdb95020057818b53891f90
Justin Geeslin <justin@geesl.in>
2018-07-18 15:14:49 -0500

    CSS Animation technique detecting for Node mutation / insertion

--- js/MarkupInit.js
+++ js/MarkupInit.js
@@ -1,27 +1,96 @@
-var markupInit = function() {
+var selectorClassMap = [
+  {
+    sel: 'input[deletable=true]',
+    class: sensible.classes.InputDelete
+  }
+];
+
+var insertableSelctors = "";
+for( var i in selectorClassMap) {
+  insertableSelctors += selectorClassMap[i].sel;
+  if (typeof selectorClassMap[i+1] !== "undefined") {
+     insertableSelctors += ', '
+  }
+}
+
+var markupInit = function( options ) {
+
+    var target = typeof options !== "undefined" ? options.target : $(document.body);
+
+    console.log('markup init')
 
     // Instanticate objects based on their markup.
-    $('div.expand-collapse').each(function() {
-      var titleEl = $(this).find('.title');
-      var bodyEl = $(this).find('.body');
-      console.log('Creating an Expand Collapse.')
-      var newElement = $(document.createDocumentFragment());
-      var anExpCol = new sensible.classes.ExpandCollapse({
-        target: newElement,
-        title: titleEl.text(),
-        content: bodyEl.html()
-      })
-      // consider never doing this. What if someone put events on this element before my replace? They'd be destroyed that's what.
-      $(this).replaceWith(newElement)
-    })
+    // $('div.expand-collapse').each(function() {
+    //   var titleEl = $(this).find('.title');
+    //   var bodyEl = $(this).find('.body');
+    //   console.log('Creating an Expand Collapse.')
+    //   var newElement = $(document.createDocumentFragment());
+    //   var anExpCol = new sensible.classes.ExpandCollapse({
+    //     target: newElement,
+    //     title: titleEl.text(),
+    //     content: bodyEl.html()
+    //   })
+    //   // consider never doing this. What if someone put events on this element before my replace? They'd be destroyed that's what.
+    //   $(this).replaceWith(newElement)
+    // })
 
-    // Init Deletable input
-    $('input[deletable=true]').each(function() {
-      var anInputDel = new sensible.classes.InputDelete({
-        el: $(this)
-      })
-    });
+    // Init Deletable input. (only those that are not already init-ed)
+    for( var i in selectorClassMap) {
+      var item = selectorClassMap[i];
+      $(item.sel + ':not([sensible-component])').each(function() {
+        var aComponent = new item.class({
+          el: $(this)
+        })
+      });
+    }
+
+
 
 }
 
-$(document).ready(markupInit)
+// Listen for an event that runs when the page is loaded and can also be triggered. (jQuery's ready cannot be triggered.)
+document.addEventListener("DOMContentLoaded", markupInit);
+
+// Listen for Body insertions using CSS and Animations in liue of their being no Mutation Observers.
+var insertListener = function(event){
+	if (event.animationName == "nodeInserted") {
+		// This is the debug for knowing our listener worked!
+		// event.target is the new node!
+		console.warn("A component has been inserted! ", event, event.target);
+    document.dispatchEvent(new Event("DOMContentLoaded"));
+	}
+}
+
+// Add animation styles to the body
+var sheet = (function() {
+	// Create the <style> tag
+	var style = document.createElement("style");
+
+	// Add a media (and/or media query) here if you'd like!
+	// style.setAttribute("media", "screen")
+	// style.setAttribute("media", "only screen and (max-width : 1024px)")
+
+	// WebKit hack :(
+	style.appendChild(document.createTextNode(""));
+
+	// Add the <style> element to the page
+	document.head.appendChild(style);
+
+	return style.sheet;
+})();
+
+function addCSSRule(sheet, selector, rules, index) {
+	if("insertRule" in sheet) {
+		sheet.insertRule(selector + "{" + rules + "}", index);
+	}
+	else if("addRule" in sheet) {
+		sheet.addRule(selector, rules, index);
+	}
+}
+
+addCSSRule(document.styleSheets[0], "@keyframes nodeInserted", "from { opacity: 0.99; } to { opacity: 1; } ");
+addCSSRule(document.styleSheets[0], insertableSelctors, "animation-duration: 0.001s; animation-name: nodeInserted;");
+
+document.addEventListener("animationstart", insertListener, false); // standard + firefox
+document.addEventListener("MSAnimationStart", insertListener, false); // IE
+document.addEventListener("webkitAnimationStart", insertListener, false); // Chrome + Safari
--- tests/MarkupInit.js
+++ tests/MarkupInit.js
@@ -1,36 +1,38 @@
 // Require all the components.
 require('../index.js')
 
-fdescribe('MarkupInit', function() {
+describe('MarkupInit', function() {
 
     beforeAll(function(done) {
+      console.log('Markup init tests.. appending')
+
       // Add expand collapse element
-      var el = $('<div class="expand-collapse"><div class="title">Untitled Title</div><div class="body">Untitled Body</div></div>');
-      $(document.body).append(el)
+      // var el = $('<div class="expand-collapse"><div class="title">Untitled Title</div><div class="body">Untitled Body</div></div>');
+      // $(document.body).append(el)
 
-      var input = $('<input type="text" deletable="true"></input>')
-      $(document.body).append(input);
 
-      $(document).trigger('ready');
 
       setTimeout(function() {
         // Wait a bit before performing tests on all the constructed elements.
         done()
       }, 100)
+
     })
 
-    it('Should init Expand Collapse', function() {
+    xit('Should init Expand Collapse', function() {
       expect($('div.expand-collapse > a').length > 0).toBe(true);
 
     });
 
-    it('Should init Input Delete', function(done) {
+    it('Should init Input Delete', function() {
+      var input = $('<input type="text" deletable="true"></input>')
+      $(document.body).append(input);
+
 
         $('input[deletable=true]').each(function() {
           // Should be wrapped in div with a class `deletable`
           expect($(this).parent().hasClass('deletable')).toBe(true);
           expect($(this).parent().find('div.close').text()).toBe('x');
-          done();
         });
 
     });
