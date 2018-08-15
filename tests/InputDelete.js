1868907ecccb24a925de10105ece771d1a190901
Justin Geeslin <justin@geesl.in>
2018-08-14 22:44:43 -0500

    New testing strategy
    Construct for every test.

--- tests/InputDelete.js
+++ tests/InputDelete.js
@@ -1,69 +1,70 @@
 describe('InputDelete', function() {
 
-    beforeAll(function(done) {
-      $(document.body).empty()
+    beforeAll(function() {
+      setupContainer = function(container) {
+        //create a list to filter.
+        container.append('<h4>Category 1</h4><ul><li>Item 1</li><li>Item 2</li><li>Item 3 <ul><li>Nested Item 1</li><li>Nested Item 2</li></ul></li></ul>');
+        container.append('<h4>Category 2</h4><ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>');
+        container.append('<h4>Category 3</h4><ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>');
+        $(document.body).append(container);
 
-      container = $('<div></div>')
-      $(document.body).append(container);
-      //create a list to filter.
-      container.append('<h4>Category 1</h4><ul><li>Item 1</li><li>Item 2</li><li>Item 3 <ul><li>Nested Item 1</li><li>Nested Item 2</li></ul></li></ul>');
-      container.append('<h4>Category 2</h4><ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>');
-      container.append('<h4>Category 3</h4><ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>');
-
-      // For testing declarative construction
-      var input = $('<input type="text" deletable="true"></input>')
-      $(document.body).append(input);
-
-      setTimeout(function() {
-        // Wait a bit before performing tests on all the constructed elements.
-        done()
-      }, 100)
+      }
 
     })
 
-    it('Should exist', function() {
-      var inputBox = $('<input type="text"></input>');
-      $(document.body).append(inputBox);
-      var theInputFilter = new sensible.classes.InputDelete({el: inputBox});
-      expect(theInputFilter.el.parent().hasClass('deletable')).toBe(true);
-    });
+    it('Should construct declaratively', function(done) {
+      var container = $('<div></div>')
+      setupContainer(container);
+      var inputBox = $('<input type="text" deletable="true"></input>')
+      container.prepend(inputBox);
 
-    it('Should construct declaratively', function() {
+      // Wait a bit for construction to happen..
+      window.setTimeout(function() {
         $('input[deletable=true]').each(function() {
           // Should be wrapped in div with a class `deletable`
           expect($(this).parent().hasClass('deletable')).toBe(true);
           expect($(this).parent().find('div.close').text()).toBe('x');
         });
-
+        done()
+      }, 100)
     });
 
-    it('should display a close icon', function() {
-      var inputBox = $('<input type="text"></input>');
-      $(document.body).append(inputBox);
-      var theInputFilter = new sensible.classes.InputDelete({el: inputBox});
-      expect(theInputFilter.el.parent().hasClass('deletable')).toBe(true);
+    it('should display a close icon', function(done) {
+      var container = $('<div></div>')
+      setupContainer(container);
+      var inputBox = $('<input type="text" deletable="true"></input>')
+      container.prepend(inputBox);
 
-      //Input text
-      inputBox.val('Item 1');
-      inputBox.trigger('input');
-      //Did the close x display?
-      expect(theInputFilter.el.parent().find('.close').css('display')).toBe('block');
+      // Wait a bit for construction to happen..
+      window.setTimeout(function() {
+        //Input text
+        inputBox.val('Item 1');
+        inputBox.trigger('input');
+        //Did the close x display?
+        expect(inputBox.parent().find('.close').css('display')).toBe('block');
+        done()
+      }, 100)
+
     });
 
     it('should clear when close icon is clicked', function() {
-      var inputBox = $('<input type="text"></input>');
-      $(document.body).append(inputBox);
-      var theInputFilter = new sensible.classes.InputDelete({el: inputBox});
-      expect(theInputFilter.el.parent().hasClass('deletable')).toBe(true);
+      var container = $('<div></div>')
+      setupContainer(container);
+      var inputBox = $('<input type="text" deletable="true"></input>')
+      container.prepend(inputBox);
 
-      //Input text
-      inputBox.val('Item Close Clearing test');
-      inputBox.trigger('input');
+      // Wait a bit for construction to happen..
+      window.setTimeout(function() {
+        //Input text
+        inputBox.val('Item Close Clearing test');
+        inputBox.trigger('input');
 
-      var closeButton = theInputFilter.el.parent().find('.close');
-      closeButton.trigger('click');
+        var closeButton = inputBox.parent().find('.close');
+        closeButton.trigger('click');
 
-      expect(inputBox.val()).toBe('');
+        expect(inputBox.val()).toBe('');
+        done()
+      }, 100)
 
     });
 
