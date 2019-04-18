b7b8f1f865fbedb1246726e8574e8c366fec0e69
Justin Geeslin <justin@geesl.in>
2019-04-17 22:41:40 -0500

    Writing an Observer test

--- tests/Observer.js
+++ tests/Observer.js
@@ -1,10 +1,22 @@
-// Is coverage so lacking as to warrant this test?
+
 describe('Sensible Observer', function() {
 
     beforeAll(function(done) {
 
     })
 
-    it('should constrct an object ')
+    it('should constrct an object', function(done) {
+      sensible.registerComponent('div.component', sensible.classes.Component);
+
+      var aComponent = $('<div class="component"></div>');
+      $(document.body).append(aComponent)
+
+      setTimeout(function() {
+        expect(aComponent[0].hasAttribute('sensible-component')).toBe(true);
+        done();
+      }, 1);
+
+
+    })
 
 });
