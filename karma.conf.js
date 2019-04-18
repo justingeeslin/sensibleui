e1ea6bcfd7972427b412e03867cecdac148b97be
Justin Geeslin <justin@geesl.in>
2019-04-17 22:47:08 -0500

    Temporarily disable special reporters, throwing errors

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage', 'html'],
+    reporters: [
+      'progress'
+      // 'coverage',
+      // 'html'
+    ],
 
     coverageReporter : {
       reporters: [
